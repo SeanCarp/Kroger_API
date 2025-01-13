242cfac0027396aba9639d4ee8c2daeac6d916de
Sean Carpenter <swcarpenter04@gmail.com>
2025-01-13 11:48:18 -0500

    Node class

--- tree_tes.py
+++ tree_tes.py
@@ -7,43 +7,9 @@
 #           - Product ID
 
 # Node Class
-import pickle, os
+import Node, pickle, os
 
 FILENAME = 'kroger_tree.pkl'
-
-class Node:
-    def __init__(self, name):
-        self.name = name
-        self.childern = []  # Needs to be instance variable
-    
-    # Checks for Duplicates
-    def add_child(self, child): 
-        print(f"trying to add {child}")
-
-        # Only adds if not found in list
-        if not child in list(map(str, self.childern)):  # Must be like this bc they aren't String (nodes)
-            self.childern.append(child)
-            print(f"{child} was added.\n")
-        else:
-            print("Child already entered")
-
-    def __str__(self) -> str:
-        return self.name
-
-
-    def child_search(self, child):
-        for item in self.childern:  # If this works, change add_child()
-            if child.lower() == item.data:
-                return item
-            
-            
-    def printTree(self, level=0):
-        print(' ' * 4 * level + '-> ' + self.name)
-
-        if len(list(map(str, self.childern))) > 0:
-            for child in self.childern:
-                child.printTree(level + 1)
-
 
 # Creates a brand new tree and empties the departments
 def root_init():
